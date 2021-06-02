dc4a7a1b06850718868b45741cdf1501771c0c4a
Simon THOBY <git@nightmared.fr>
2021-06-02 20:26:22 +0200

    bump rust version from 1.51 to 1.52

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 self: super:
 let
-  pkgs = import <nixpkgs> {};
-  rustPlatform = pkgs.makeRustPlatform {
-    cargo = pkgs.rust_1_51.packages.stable.cargo;
-    rustc = pkgs.rust_1_51.packages.stable.rustc;
-  };
+pkgs = import <nixpkgs> {};
+rustPlatform = pkgs.makeRustPlatform {
+    cargo = pkgs.rust_1_52.packages.stable.cargo;
+    rustc = pkgs.rust_1_52.packages.stable.rustc;
+};
 in
 {
-  s3s = import ./s3s.nix { inherit pkgs rustPlatform; };
-  availcheck = import ./availcheck.nix { inherit pkgs rustPlatform; };
-  le-dns = import ./le-dns.nix { inherit pkgs rustPlatform; };
+    s3s = import ./s3s.nix { inherit pkgs rustPlatform; };
+    availcheck = import ./availcheck.nix { inherit pkgs rustPlatform; };
+    le-dns = import ./le-dns.nix { inherit pkgs rustPlatform; };
 }
